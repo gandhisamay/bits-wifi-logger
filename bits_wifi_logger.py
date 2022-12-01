9470e0852c642ef3c151a383c0c1921a5ca09e73
Samay Gandhi <79245578+gandhisamay@users.noreply.github.com>
2022-12-01 15:23:54 +0530

    OOP implementation

--- bits_wifi_logger.py
+++ bits_wifi_logger.py
@@ -4,38 +4,52 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 
-driver = webdriver.Chrome(service=Service('/home/samaygandhi/Documents/chromedriver'))
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--incognito')
-driver = webdriver.Chrome(options=options)
+class BitsWifiLogger:
+    def __init__(self):
+        driver_path = "/home/samaygandhi/Documents/chromedriver"
+        options = webdriver.ChromeOptions()
+        options.binary_location = "/usr/bin/brave-browser"
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--incognito')
+        self.DRIVER = webdriver.Chrome(service=Service(executable_path=driver_path), options=options)
 
-driver.get("https://fw.bits-pilani.ac.in:8090")
+    def get_keys(self):
+        #Extract the Credentials
+        exists = os.path.isfile("/home/samaygandhi/Documents/.env")
+        if exists:
+            load_dotenv()
+            self.USERNAME = os.environ.get('USERNAME')
+            self.PASSWORD = os.environ.get('PASSWORD') 
+        else: 
+            raise Exception('Credentials not found')
 
 
-exists = os.path.isfile("/home/samaygandhi/Documents/.env")
-if exists:
-    # Extract the details
-    load_dotenv()
-    usnm = os.environ.get('USERNAME')
-    pswd = os.environ.get('PASSWORD') 
-else: 
-    raise Exception('Credentials not found')
+    def run(self):
+        #Get the website
+        self.DRIVER.get("https://fw.bits-pilani.ac.in:8090")
 
-username = driver.find_element(By.ID,'username')
-password = driver.find_element(By.ID,'password')
-username.send_keys(usnm)
-password.send_keys(pswd)
+        self.get_keys()
 
-driver.find_element(By.ID, 'loginbtn').click()
+        #Send the keys to the appropriate the input fields
+        
+        username = self.DRIVER.find_element(By.ID,'username')
+        password = self.DRIVER.find_element(By.ID,'password')
 
-driver.implicitly_wait(time_to_wait=1)
+        username.send_keys(self.USERNAME)
+        password.send_keys(self.PASSWORD)
 
-connectionStatusMessage = driver.find_element(By.ID, 'statusmessage').text
+        #Connect
+        self.DRIVER.find_element(By.ID, 'loginbtn').click()
+        self.DRIVER.implicitly_wait(time_to_wait=1)
 
-if connectionStatusMessage:
-    print('Failed to connect')
-else:
-    print('Connected Successfully')
+        connectionStatusMessage = self.DRIVER.find_element(By.ID, 'statusmessage').text
 
-driver.close()
+        if connectionStatusMessage: print('Failed to connect')
+        else:print('Connected Successfully')
+
+        self.DRIVER.close()
+
+
+if __name__ == "__main__":
+    logger = BitsWifiLogger()
+    logger.run()
