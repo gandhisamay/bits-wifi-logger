08d6b53b0810c815e63c7dfcaad8b16d25e66e71
Samay Gandhi <79245578+gandhisamay@users.noreply.github.com>
2023-01-02 13:41:56 +0530

    Add: wifi check before running logger.

--- bits_wifi_logger.py
+++ bits_wifi_logger.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
+import subprocess
 
 class BitsWifiLogger:
     def __init__(self):
@@ -11,8 +12,16 @@ class BitsWifiLogger:
         options.binary_location = "/usr/bin/brave-browser"
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('--incognito')
+        options.add_argument('--headless')
         self.DRIVER = webdriver.Chrome(service=Service(executable_path=driver_path), options=options)
 
+    def get_current_connection(self):
+        self.BITS_WIFI = "BITS STUDENT"
+        ssid = str(subprocess.run(["iwgetid", "-r"],capture_output=True).stdout)
+        if self.BITS_WIFI in ssid:
+            self.WIFI_NAME = self.BITS_WIFI
+        else: self.WIFI_NAME = "OTHERS"
+       
     def get_keys(self):
         #Extract the Credentials
         exists = os.path.isfile("/home/samaygandhi/Documents/.env")
@@ -26,29 +35,35 @@ class BitsWifiLogger:
 
     def run(self):
         #Get the website
-        self.DRIVER.get("https://fw.bits-pilani.ac.in:8090")
+        try:
+            self.get_current_connection()
 
-        self.get_keys()
+            if self.WIFI_NAME != self.BITS_WIFI: 
+               os.execlp("echo", "echo", "Bits wifi not connected")
 
-        #Send the keys to the appropriate the input fields
-        
-        username = self.DRIVER.find_element(By.ID,'username')
-        password = self.DRIVER.find_element(By.ID,'password')
+            self.DRIVER.get("https://fw.bits-pilani.ac.in:8090")
+            self.get_keys()
 
-        username.send_keys(self.USERNAME)
-        password.send_keys(self.PASSWORD)
+            #Send the keys to the appropriate the input fields
+            
+            username = self.DRIVER.find_element(By.ID,'username')
+            password = self.DRIVER.find_element(By.ID,'password')
 
-        #Connect
-        self.DRIVER.find_element(By.ID, 'loginbtn').click()
-        self.DRIVER.implicitly_wait(time_to_wait=1)
+            username.send_keys(self.USERNAME)
+            password.send_keys(self.PASSWORD)
 
-        connectionStatusMessage = self.DRIVER.find_element(By.ID, 'statusmessage').text
+            #Connect
+            self.DRIVER.find_element(By.ID, 'loginbtn').click()
+            self.DRIVER.implicitly_wait(time_to_wait=1)
 
-        if connectionStatusMessage: print('Failed to connect')
-        else:print('Connected Successfully')
+            connectionStatusMessage = self.DRIVER.find_element(By.ID, 'statusmessage').text
+            self.DRIVER.close()
 
-        self.DRIVER.close()
-
+            if connectionStatusMessage: os.execlp("notify-send", "notify-send", "-u", "low", "bits_wifi_logger: Login Failed!")
+            else:os.execlp("notify-send", "notify-send", "-u", "low", "bits_wifi_logger: Login Successfull")
+        except:
+           self.DRIVER.close()
+           os.execlp("notify-send", "notify-send", "-u", "low", "bits_wifi_logger: Login Failed!")
 
 if __name__ == "__main__":
     logger = BitsWifiLogger()
